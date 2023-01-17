e6699650239e5a231dbc2d079154ecee99c1147b
BerenyiPeti <berenyipeti01@gmail.com>
2023-01-17 16:17:38 +0100

    home, ln, chg, lnd componensek

---
+++ src/app/chg/chg.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chg',
+  templateUrl: './chg.component.html',
+  styleUrls: ['./chg.component.css']
+})
+export class ChgComponent {
+
+}
