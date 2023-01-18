fa814556d8df9bdb16c21c0d5c253869d408f000
KncskBenedek <koncsikbenedek@gmail.com>
2023-01-18 18:58:50 +0100

    alap routok működnek

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,26 +9,28 @@ import { LndComponent } from './lnd/lnd.component';
 import { ChgComponent } from './chg/chg.component';
 import { WoComponent } from './wo/wo.component';
 import { XWoCoponent } from './xwo/xwo.component';
+import { LdComponent } from './ld/ld.component';
+import { UserComponent } from './user/user.component';
 
 
 const routes: Routes = [
-  {path:'', redirectTo: '/home', pathMatch: 'full'},
-  {path:'/home', component: HomeComponent},
-  {path: '/auth', component: AuthComponent},
-  {path: '/part', children:[
+  {path:'', redirectTo: 'home', pathMatch: 'full'},
+  {path:'home', component: HomeComponent},
+  {path: 'auth', component: AuthComponent},
+  {path: 'part', children:[
     {path: '', component:  PtComponent},
     {path: 'structure', component: PsComponent}
   ]}, 
-  {path: '/part/structure', component: PsComponent},
-  {path: '/inventory', component: PsComponent},
-  {path: '/line', children: [
+  //{path: 'part/structure', component: PsComponent},
+  {path: 'inventory', component: LdComponent},
+  {path: 'line', children: [
     {path: '', component: LnComponent},
     {path: 'change', component: ChgComponent},
     {path: 'rate', component: LndComponent},
   ]},
   {path: 'workorder', component: WoComponent}, 
   {path: 'prodsch', component: XWoCoponent},
-  {path: 'user', component: XWoCoponent}
+  {path: 'user', component: UserComponent}
 
 
 ];
