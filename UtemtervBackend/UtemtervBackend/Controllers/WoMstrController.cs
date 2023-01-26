fd657240b4030121688e5b905a1359f4a311624d
KncskBenedek <koncsikbenedek@gmail.com>
2023-01-26 14:13:43 +0100

    WoMstr controller

---
+++ UtemtervBackend/UtemtervBackend/Controllers/WoMstrController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UtemtervBackend.Models;
+
+namespace UtemtervBackend.Controllers
+{
+    [Route("workoder")]
+    [ApiController]
+    public class WoMstrController : ControllerBase
+    {
+        private UtemtervContext _context;
+        public WoMstrController(UtemtervContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("list")]
+        public IActionResult WoList()
+        {
+            try
+            {
+                var woList = _context.WoMstrs.FromSqlRaw("woList").AsEnumerable();
+               
+
+                if (woList.Count() == 0)
+                {
+                    return NotFound("Workoder not found.");
+                }
+                return Ok(woList);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(500, "An Error has occured.");
+            }
+        }
+
+        
+    }
+}
