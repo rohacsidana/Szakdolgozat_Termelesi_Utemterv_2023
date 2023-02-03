777c6c37e5a07cb0f355677a99cc2d189ec01c5f
rohacsidana <rohacsi.dana@gmail.com>
2023-02-03 15:57:16 +0100

    pt api végpontok, mapparendzser

---
+++ UtemtervBackend/UtemtervBackend/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UtemtervBackend.Models;
+
+namespace UtemtervBackend.Controllers
+{
+    [EnableCors]
+    [Route("pt")]
+    [ApiController]
+    public class PtController : ControllerBase
+    {
+        UtemtervContext _context;
+
+        public PtController(UtemtervContext context)
+        {
+            _context = context;
+        }
+
+        [EnableCors]
+        [HttpGet("list")]
+
+        public IActionResult PtList()
+        {
+            try
+            {
+                var ptList = _context.PtMstrs.ToList();
+
+                if(ptList.Count() == 0)
+                {
+                    return NotFound("Pt not found.");
+                }
+                return Ok(ptList);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [EnableCors]
+        [HttpPost("new")]
+    }
+}
