89cc48f11cc383fce6f8781a1c571cbbab459305
rohacsidana <rohacsi.dana@gmail.com>
2023-03-01 16:12:50 +0100

    poszt nevek megváltoztazása

--- UtemtervFrontend/src/app/home/home.component.ts
+++ UtemtervFrontend/src/app/home/home.component.ts
@@ -4,60 +4,55 @@ import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit, OnDestroy{
+export class HomeComponent implements OnInit, OnDestroy {
   userName: string = null;
-  splitName: string[]
-  firstName: string
-  post: number
-  postName: string
-  id: number
+  splitName: string[];
+  firstName: string;
+  post: number;
+  postName: string;
+  id: number;
 
   userSub: Subscription;
-  constructor(private authService: AuthService) {
-
-  }
+  constructor(private authService: AuthService) {}
   ngOnInit(): void {
-    this.userSub = this.authService.user.subscribe((data)=>{
-      if(data != null){
-        this.userName = data.name
-      }else{
-        this.userName = null
+    this.userSub = this.authService.user.subscribe((data) => {
+      if (data != null) {
+        this.userName = data.name;
+      } else {
+        this.userName = null;
       }
-      this.post = data.post
-      this.id = data.id
-    })
+      this.post = data.post;
+      this.id = data.id;
+    });
 
-    this.splitName = this.userName.split(/\s+/)
+    this.splitName = this.userName.split(/\s+/);
 
     if (this.splitName.length > 1) {
-      this.firstName = this.splitName[1]
+      this.firstName = this.splitName[1];
     }
 
     if (this.splitName.length == 1) {
-      this.firstName = this.splitName[0]
+      this.firstName = this.splitName[0];
     }
 
-    switch(this.post) { 
-      case 1: { 
-         this.postName = 'Termelés vezető beosztott'
-         break; 
-      } 
-      case 2: { 
-        this.postName = 'Készletgazda beosztott'
-         break; 
-      } 
-      case 3: { 
-        this.postName = 'Admin'
-         break; 
-      } 
-   } 
-    
-    
-     
+    switch (this.post) {
+      case 1: {
+        this.postName = 'Admin';
+        break;
+      }
+      case 2: {
+        this.postName = 'Készletgazda beosztott';
+        break;
+      }
+      case 3: {
+        this.postName = 'Termelés vezető beosztott';
+        break;
+      }
+    }
   }
- ngOnDestroy(): void {
-   this.userSub.unsubscribe();
- }
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
 }
