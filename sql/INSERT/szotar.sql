12dbcba6a29028241b8550fdf986ea854e7e5d18
KncskBenedek <koncsikbenedek@gmail.com>
2023-03-03 11:40:44 +0100

    javított óránkénti egység sql

---
+++ sql/INSERT/szotar.sql
@@ -0,0 +1,4 @@
+insert into DICTIONARY values('waiting','wo_status',null);
+insert into DICTIONARY values('accepted','wo_status',null);
+insert into DICTIONARY values('ongoing','wo_status',null);
+insert into DICTIONARY values('completed','wo_status',null);
--- sql/functions/orankentiEgyseg.sql
+++ sql/functions/orankentiEgyseg.sql
@@ -7,19 +7,19 @@ as
 begin
 	declare @egys decimal(18,5);
 
-	with seged (par, part,lehet, /*lehet_req,*/ e_req,/* rate,*/ szint)
+	with seged (par, part,lehet, e_req, szint)
 	as
 	(
-		select pt.pt_part as par, pt.pt_part as part, cast(lnd.lnd_rate as decimal(18,5)) as lehet, /*lnd.lnd_rate as lehet_req,*/ cast(1 as decimal(18,5)) as e_req, /*lnd.lnd_rate as rate,*/ 0 as szint
+		select pt.pt_part as par, pt.pt_part as part, cast(lnd.lnd_rate as decimal(18,5)) as lehet,  cast(lnd.lnd_rate  as decimal(18,5)) as e_req, 0 as szint
 		from pt_mstr pt inner join lnd_det lnd on pt.pt_part = lnd.lnd_part
+
 		where pt.pt_part = @part
 		and lnd.lnd_line = @line
 		union all
 		select ps.ps_par, ps.ps_comp
-		,  cast( iif(ps.ps_qty_per * s.lehet < lnd.lnd_rate, s.lehet, lnd.lnd_rate/(ps.ps_qty_per * s.e_req) ) as decimal(18,5) ) --lehet gyártani menny.
-		--, cast(ps.ps_qty_per * iif(ps.ps_qty_per * lehet < lnd.lnd_rate, lehet, lnd.lnd_rate/ps.ps_qty_per * e_req ) as decimal(18,5)) --lehet-hez szüks mennyi
-		,cast(ps.ps_qty_per * s.e_req as decimal(18,5)) -- egy hez szükséges mennyi
-		--,lnd.lnd_rate
+		,  cast( iif(ps.ps_qty_per * s.e_req <= lnd.lnd_rate, s.lehet, s.lehet * lnd.lnd_rate/(ps.ps_qty_per * s.e_req) ) as decimal(18,5) ) 
+		--,cast(ps.ps_qty_per * iif(ps.ps_qty_per * s.e_req <= lnd.lnd_rate, s.e_req,s.e_req  * (lnd.lnd_rate/(ps.ps_qty_per * s.e_req))) as decimal(18,5)) 
+		,lnd.lnd_rate
 		, s.szint + 1
 		from ps_mstr ps inner join seged s on ps.ps_par = s.part
 						inner join lnd_DET lnd on ps.ps_comp = lnd.lnd_part
@@ -27,7 +27,6 @@ begin
 	)
 	select @egys = min(s.lehet)
 	from seged s
-	where s.szint = (select max(sb.szint) from seged sb)
 	return @egys;
 	
 end
