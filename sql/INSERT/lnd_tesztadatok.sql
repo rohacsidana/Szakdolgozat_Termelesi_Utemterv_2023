61ca4bed69ba10bd64c1e9fb4f9fadf0a05b6e5b
BerenyiPeti <berenyipeti01@gmail.com>
2023-03-07 12:01:25 +0100

    lnd, chg tesztadatok

---
+++ sql/INSERT/lnd_tesztadatok.sql
@@ -0,0 +1,9 @@
+use Utemterv
+go
+
+insert into LND_DET values('line_01', 1000, 200)
+insert into LND_DET values('line_01', 1002, 250)
+insert into LND_DET values('line_01', 1003, 300)
+insert into LND_DET values('line_02', 1020, 400)
+insert into LND_DET values('line_02', 1021, 600)
+insert into LND_DET values('line_02', 1026, 500)
