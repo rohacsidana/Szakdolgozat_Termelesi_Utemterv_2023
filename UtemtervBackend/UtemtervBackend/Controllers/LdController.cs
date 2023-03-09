8ce6cc9a75e76f97ad56c45871396fbdb78d5c2b
rohacsidana <rohacsi.dana@gmail.com>
2023-03-09 10:25:00 +0100

    role-ok hozzáadása: pt, pt, ld

--- UtemtervBackend/UtemtervBackend/Controllers/LdController.cs
+++ UtemtervBackend/UtemtervBackend/Controllers/LdController.cs
@@ -23,7 +23,7 @@ namespace UtemtervBackend.Controllers
 
         [EnableCors]
         [HttpGet("list")]
-
+        [Authorize(Roles = "1,3")]
         public IActionResult LdList()
         {
             try
@@ -42,6 +42,7 @@ namespace UtemtervBackend.Controllers
         }
 
         [HttpPost("new")]
+        [Authorize(Roles = "1,3")]
         public IActionResult newLd([FromBody] NewOrUpdateLd ld)
         {
             try
@@ -55,6 +56,7 @@ namespace UtemtervBackend.Controllers
         }
 
         [HttpDelete("delete/{part}/{exp}")]
+        [Authorize(Roles = "1,3")]
 
         public IActionResult deleteLd(int part, int exp)
         {
@@ -69,6 +71,7 @@ namespace UtemtervBackend.Controllers
         }
 
         [HttpPut("update")]
+        [Authorize(Roles = "1,3")]
 
         public IActionResult updateLd([FromBody] NewOrUpdateLd ld)
         {
