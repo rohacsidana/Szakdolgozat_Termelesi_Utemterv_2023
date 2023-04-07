45c114819f4bd569ed24a23185b8811ddbcc275a
KncskBenedek <koncsikbenedek@gmail.com>
2023-04-07 17:46:39 +0200

    lad error message és loading spinner beépítve, működik

--- UtemtervFrontend/src/app/reserve/lad-form.component.ts
+++ UtemtervFrontend/src/app/reserve/lad-form.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { DataTableService } from '../data-table/data-table.service';
 import { DataStorageService } from '../shared/data-storage.service';
 import { ReserveService } from './reserve.service';
+import { tap } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-lad-form',
@@ -10,6 +13,8 @@ import { ReserveService } from './reserve.service';
 })
 export class LadFormComponent {
   womHeader = [];
+  isLoading: boolean = false;
+  error: string = null;
   search(lot) {}
 
   constructor(
@@ -19,6 +24,7 @@ export class LadFormComponent {
   ) {}
 
   reserve(ladPart, ladPar, LadLot, ladComp, ladExpire, ladAmount) {
+    this.isLoading = true;
     this.DataStorageService.reserve(
       ladPart,
       ladPar,
@@ -26,8 +32,37 @@ export class LadFormComponent {
       ladComp,
       ladExpire,
       ladAmount
-    ).subscribe((res) => {
-      console.log(res);
-    });
+    )
+      .pipe(
+        tap({
+          next: (res) => {
+            console.log(res);
+            this.isLoading = false;
+          },
+          error: (err) => {
+            this.isLoading = false;
+              this.handleError(err);
+          },
+        })
+      )
+      .subscribe();
+  }
+
+  handleError(errorRes: HttpErrorResponse){
+    let errorMessage = 'Ismeretlen hiba történt.';
+
+    switch (errorRes.error) {
+      case "INCORRECT_PARAM":
+        errorMessage = "Nem megfelelő adatokat adott meg."
+        break;
+
+    }
+    
+    this.error = errorMessage;
+    return throwError(errorMessage);
+  }
+
+  onHandleError(){
+    this.error = null;
   }
 }
